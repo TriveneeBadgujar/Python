3d5b81abf50cfa5af14c98419a8153b19eafd018
Trivenee Badgujar <161133137+TriveneeBadgujar@users.noreply.github.com>
2026-07-07 10:59:21 +0530

    Create Comment.py

---
+++ Comment.py
@@ -0,0 +1,14 @@
+#Given three lines that print the values of a, b, and c. Comment out the line that prints the value of b.
+#Comments are useful in any programming language to describe the purpose of functions or operations. They are ignored by the compiler, so they do not affect the execution of the code.
+a = int(input())
+b = int(input())
+c = int(input())
+
+# Prints a
+print(a)
+
+# Prints b
+#print(b)
+
+# Prints c
+print(c)
