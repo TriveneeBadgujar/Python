1d351f891977db0dd3fdb6c04523e0ab7b79312d
Trivenee Badgujar <161133137+TriveneeBadgujar@users.noreply.github.com>
2026-07-07 11:01:12 +0530

    Create TakingInput.py

---
+++ TakingInput.py
@@ -0,0 +1,22 @@
+#You need to perform three separate tasks based on the given input:
+"""
+String Input and Print: Read a string s (which may contain spaces) and print it as it is.
+Integer Input and Print: Read an integer n and print it without any change.
+Float Input and floor Print: Read a floating-point number as input, take its floor value, and print as an integer.
+"""
+import math
+
+# s to store string
+# n to store integer
+# f to store float
+# ff  # To Store floor of float variable f
+
+# code here
+s= input()
+n= int(input())
+f= float(input())
+ff=math.floor(f)
+
+print(s)
+print(n)
+print(ff)
