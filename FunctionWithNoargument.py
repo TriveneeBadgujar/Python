257cbdc8fa183e87781cc181036216f812596478
Trivenee Badgujar <161133137+TriveneeBadgujar@users.noreply.github.com>
2026-07-07 18:46:08 +0530

    Create FunctionWithNoargument.py

---
+++ FunctionWithNoargument.py
@@ -0,0 +1,6 @@
+#This problem has no input. You need to write the function helloFunction that prints Hello.
+#  Write the helloFunction below to print "Hello"
+
+# code here
+def helloFunction():
+    print("Hello")
