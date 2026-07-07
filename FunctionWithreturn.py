f26f2d30a58cba67a50172f0212fe1c054777655
Trivenee Badgujar <161133137+TriveneeBadgujar@users.noreply.github.com>
2026-07-07 18:41:18 +0530

    Create FunctionWithreturn.py

---
+++ FunctionWithreturn.py
@@ -0,0 +1,6 @@
+#Given an integer n, You need to use the return keyword to return twice the value of n.
+class Solution:
+    def returnValueFunction(self, n):
+        # code here
+        return n*2
+        
