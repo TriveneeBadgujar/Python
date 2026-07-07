c2b27b72b5384ce3b2d16f3446e79500767fb7ff
Trivenee Badgujar <161133137+TriveneeBadgujar@users.noreply.github.com>
2026-07-07 18:51:58 +0530

    Create FunctionWithArguments.py

---
+++ FunctionWithArguments.py
@@ -0,0 +1,6 @@
+# When Class method performed then self parameter needed
+#Given two integers a and b. The given input and its values are passed as arguments to the function argumentFunction. The argumentFunction is responsible to return (a+b). You need to write this function.
+class Solution:
+    # code here
+    def argumentFunction(self,a,b):
+        return a + b
