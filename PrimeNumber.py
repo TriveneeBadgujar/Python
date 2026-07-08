1947bcd4c8fa5cb45145b58584c26c313beb10bc
Trivenee Badgujar <161133137+TriveneeBadgujar@users.noreply.github.com>
2026-07-08 10:56:38 +0530

    Create PrimeNumber.py

---
+++ PrimeNumber.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPrime(self, n):
+        if n <= 1:
+            return False
+
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+
+        return True
+#other way
+n = int(input())
+
+if n <= 1:
+    print("false")
+else:
+    prime = True
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            prime = False
+            break
+
+    print("true" if prime else "false")
